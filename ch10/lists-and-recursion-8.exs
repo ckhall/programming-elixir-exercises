1712ca2514ece0b60c16c855b9081e1ee6ac4ff8
Chris Hall <christopher.k.hall@gmail.com>
2016-03-20 00:01:48 -0400

    tweaks

--- ch10/lists-and-recursion-8.exs
+++ ch10/lists-and-recursion-8.exs
@@ -8,17 +8,20 @@ defmodule Taxes do
   # ]
 
   def calculate(tax_rates, orders) do
-    IO.inspect Enum.map(orders, &(apply_tax(&1, tax_rates)))
+    Enum.map(orders, &(&1 |> apply_tax(tax_rates)))
   end
 
   def apply_tax(order, tax_rates) do
     [ id: _, ship_to: state, net_amount: net ] = order
-    tax_rate = Keyword.get(tax_rates, state, 0)
-    total = net |> calc_total(tax_rate)
+    total = state |> tax_rate(tax_rates) |> calc_total(net)
     order ++ [ total_amount: total ]
   end
 
-  def calc_total(net, rate) do
+  def tax_rate(state, tax_rates) do
+    Keyword.get(tax_rates, state, 0)
+  end
+
+  def calc_total(rate, net) do
     net * (1.0 + rate) |> Float.round(2)
   end
 end
